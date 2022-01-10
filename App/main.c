3b0ef7b217a0a4fac8dd7aac4e9e14718eeb0d86
gutianxiang <gu_tianxiang@simware.cn>
2022-01-10 15:28:27 +0800

    add timers_init

--- App/main.c
+++ App/main.c
@@ -4,6 +4,8 @@
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 //end
+//timer need head file
+#include "app_timer.h"
 
 static void log_init(void)
 {
@@ -14,9 +16,18 @@ static void log_init(void)
 	NRF_LOG_DEFAULT_BACKENDS_INIT();
 }
 
+static void timers_init(void)
+{
+	//init timer
+	ret_code_t err_code = app_timer_init();
+	APP_ERROR_CHECK(err_code);
+	//add user code
+}
+
 int main(void)
 {
 	log_init();
+	timers_init();
 	NRF_LOG_INFO("test started.");
 	while(1)
 	{
